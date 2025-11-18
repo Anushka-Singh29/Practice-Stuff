b8b9b316487321fe26ab67cbc30c6cdc5d59097d
Anushka <899anushkasingh@gmail.com>
2025-11-18 15:21:33 +0530

    minor addons to part 1

--- 10contextMini/components/Home.jsx
+++ 10contextMini/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../src/context/UserContext'
+
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const {user} = useContext(UserContext)
+
+    return (
+        <div>
+            <div className="card" style={{ width: 30 + 'rem' }}>
+                <img className="card-img-top" src="" />
+                <div className="card-body">
+                    <h1 className="card-title">{user.username}</h1>
+                    <h2 className="card-text">Lorem ipsum dolor sit. </h2>
+                    <h4 className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laborum soluta itaque autem rem! </h4>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
--- 10contextMini/src/App.jsx
+++ 10contextMini/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import UserContextProvider from './context/userContextProvider'
 import Login from '../components/Login'
 import Profile from '../components/Profile'
+import Home from '../components/Home'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <h1>React and coding</h1>
       <Login />
       <Profile />
+      <Home />
     </UserContextProvider>
   )
 }
