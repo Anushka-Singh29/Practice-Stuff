272cbbc6a89407fb5fed02aa6c37b17805ba5bfa
Anushka <899anushkasingh@gmail.com>
2025-11-19 14:26:53 +0530

    Routing and ContextAPI

--- 10contextMini/components/Profile.jsx
+++ 10contextMini/components/Profile.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import UserContext from '../src/context/UserContext'
+import {Link} from 'react-router-dom'
+import Home from './Home'
 
 
 const Profile = () => {
@@ -14,8 +16,10 @@ const Profile = () => {
     return(
         <div>
             Welcome {user.username}
+            <Link to='/home'>Home page</Link>
         </div>
     )
+
  
 }
 
--- 10contextMini/src/App.jsx
+++ 10contextMini/src/App.jsx
@@ -3,15 +3,22 @@ import UserContextProvider from './context/userContextProvider'
 import Login from '../components/Login'
 import Profile from '../components/Profile'
 import Home from '../components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 const App = () => {
   return (
     <UserContextProvider>
+      <BrowserRouter>
+      
       <h1>React and coding</h1>
       <Login />
       <Profile />
-      <Home />
+      <Routes>
+        <Route path='home' element={<Home />} />
+      </Routes>
+      {/* <Home /> */}
+      </BrowserRouter>
     </UserContextProvider>
   )
 }
